ed13771e9a6c34fdd02e8322991ac2a64d91d322
ousia-yang <105339226+ousia-yang@users.noreply.github.com>
2022-05-18 17:36:26 -0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
   AUTHOR: 'OUSIA.', // 作者
-  BIO: '一个自认为有良知和原则的原创作者、金属头、艾欧泽亚居民。婉拒一切软硬性质的传教，谢谢合作。', // 作者简介
+  BIO: '一个自认为有良知和原则的原创作者、金属头、艾欧泽亚居民', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
